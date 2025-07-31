eaf2a5d58176f2fe772f8c65c99d27496eb5220c
Mesi1337 <103388239+Mesi1337@users.noreply.github.com>
2025-07-31 11:14:48 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'pompki-cache-v1';
+const urlsToCache = [
+  'tracker-pompek.html',
+  'manifest.json',
+  'icon-192.png',
+  'icon-512.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.9.1/chart.min.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
